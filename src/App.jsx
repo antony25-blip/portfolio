963c206fd3079bca2b6887a83e6e9ad81bd4ee4c
antony25-blip <antonysmechery25@gmail.com>
2025-08-09 22:24:54 +0530

    Fix case-sensitive import for Navbar to render on GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
 import Skills from './components/Skills'
